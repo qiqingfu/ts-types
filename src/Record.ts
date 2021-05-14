0f9e96a26d5be6727efa49bca0715f8e6578f43a
qiqf <723469354@qq.com>
2021-05-14 13:34:18 +0800

    docs(src): Documents in English and Chinese

---
+++ src/Record.ts
@@ -0,0 +1,18 @@
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeystype
+
+type MyRecord<T extends string | number | symbol, U> = {
+  [P in T]: U
+}
+
+interface CatInfo {
+  age: number
+  breed: string
+}
+
+type CatName = 'miffy' | 'boris' | 'mordred'
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: 'Persian' },
+  boris: { age: 5, breed: 'Maine Coon' },
+  mordred: { age: 16, breed: 'British Shorthair' }
+}
