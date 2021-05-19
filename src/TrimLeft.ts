2916b4a895bedb1c90f53f48cb6f4ea276bba859
qiqf <723469354@qq.com>
2021-05-19 23:01:35 +0800

    feat: add TrimLeft

--- src/TrimLeft.ts
+++ src/TrimLeft.ts
@@ -1,1 +1,8 @@
 // https://github.com/type-challenges/type-challenges/blob/master/questions/106-medium-trimleft/README.md
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-1.html#template-literal-types
+
+type Space = ' ' | '\n' | '\t'
+
+export type TrimLeft<T extends string> = T extends `${Space}${infer R}` ? TrimLeft<R> : T
+
+type Str = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
